c960ce7156b86c46b2f7155e016d7a1aad449d62
romain <romain.dortier@gmail.com>
2017-02-14 15:02:03 +0100

    Changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,11 +18,128 @@ namespace :deploy do
         end
   end
 
-  desc "Run containers"
-  task :dockerup do
+  desc "Varnish"
+  task :varnish_up do
       on release_roles :all do
           within release_path do
-              execute :'docker-compose', :up, "--build", "-d"
+              # execute :'docker', :stop, 'varnish'
+              # execute :'docker', :rm, 'varnish'
+              execute :'docker-compose', :up, '--build', '-d', 'varnish'
+          end
+      end
+  end
+
+  desc "Nginx"
+  task :nginx_up do
+      on release_roles :all do
+          within release_path do
+              # execute :'docker', :stop, 'nginx'
+              # execute :'docker', :rm, 'nginx'
+              execute :'docker-compose', :up, '--build', '-d', 'nginx'
+          end
+      end
+  end
+
+  desc "Redis"
+  task :redis_up do
+      on release_roles :all do
+          within release_path do
+              # execute :'docker', :stop, 'redis'
+              # execute :'docker', :rm, 'redis'
+              execute :'docker-compose', :up, '--build', '-d', 'redis'
+          end
+      end
+  end
+
+  desc "Phpmyadmin"
+  task :phpmyadmin_up do
+      on release_roles :all do
+          within release_path do
+              # execute :'docker', :stop, 'phpmyadmin'
+              # execute :'docker', :rm, 'phpmyadmin'
+              execute :'docker-compose', :up, '--build', '-d', 'phpmyadmin'
+          end
+      end
+  end
+
+  desc "Mongo"
+  task :mongo_up do
+      on release_roles :all do
+          within release_path do
+              # execute :'docker', :stop, 'mongo'
+              # execute :'docker', :rm, 'mongo'
+              execute :'docker-compose', :up, '--build', '-d', 'mongo'
+          end
+      end
+  end
+
+  desc "Mysql"
+  task :mysql_up do
+      on release_roles :all do
+          within release_path do
+              # execute :'docker', :stop, 'mysql'
+              # execute :'docker', :rm, 'mysql'
+              execute :'docker-compose', :up, '--build', '-d', 'mysql'
+          end
+      end
+  end
+
+
+  desc "Php fpm"
+  task :phpfpm_up do
+      on release_roles :all do
+          within release_path do
+              # execute :'docker', :stop, 'php-fpm'
+              # execute :'docker', :rm, 'php-fpm'
+              execute :'docker-compose', :up, '--build', '-d', 'php-fpm'
+          end
+      end
+  end
+
+
+  desc "Jenkins"
+  task :jenkins_up do
+      on release_roles :all do
+          within release_path do
+              # execute :'docker', :stop, 'jenkins'
+              # execute :'docker', :rm, 'jenkins'
+              execute :'docker-compose', :up, '--build', '-d', 'jenkins'
+          end
+      end
+  end
+
+
+  desc "Applications"
+  task :applications_up do
+      on release_roles :all do
+          within release_path do
+              # execute :'docker', :stop, 'applications'
+              # execute :'docker', :rm, 'applications'
+              execute :'docker-compose', :up, '--build', '-d', 'applications'
+          end
+      end
+  end
+
+
+  desc "Workspace"
+  task :workspace_up do
+      on release_roles :all do
+          within release_path do
+              # execute :'docker', :stop, 'workspace'
+              # execute :'docker', :rm, 'workspace'
+              execute :'docker-compose', :up, '--build', '-d', 'workspace'
+          end
+      end
+  end
+
+
+  desc "Rabbitmq"
+  task :rabbitmq_up do
+      on release_roles :all do
+          within release_path do
+              # execute :'docker', :stop, 'rabbitmq'
+              # execute :'docker', :rm, 'rabbitmq'
+              execute :'docker-compose', :up, '--build', '-d', 'rabbitmq'
           end
       end
   end
